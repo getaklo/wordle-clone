72d9cf8b27d65fd9568ba701ceccdfae03c4d370
georgkt <georgtaklo@gmail.com>
2024-03-04 14:22:07 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import toast from "react-hot-toast";
 import { IoReload } from "react-icons/io5";
 
 function App() {
-  //const [word, setWord] = useState<{ letter: string; color: string }[]>([]);
   const [guess, setGuess] = useState<string>("");
   const [actualWord, setActualWord] = useState<string>("hello");
   const [attempt, setAttempt] = useState<number>(0);
@@ -15,21 +14,19 @@ function App() {
     getRandomWord()
       .then((word) => {
         setActualWord(word);
-        //setWord(new Array(5).fill({ letter: "", color: "bg-gray-500" }));
       })
       .catch((error) => console.error(error));
   }, []);
 
   useEffect(() => {
     if (attempt === 5) {
-      toast.error(
-        "You have reached the maximum number of attempts. The word was " +
-        actualWord
-        );
-        setTimeout(() => {window.location.reload()}, 3000);
+      toast.error("Sorry, all attempts are used :( The word was " + actualWord);
+      setTimeout(() => {
+        window.location.reload();
+      }, 3000);
       return;
     }
-  },[attempt]);
+  }, [attempt]);
 
   function checkGuess() {
     let newWord = new Array(5);
@@ -41,72 +38,79 @@ function App() {
     for (let i = 0; i < guess.length; i++) {
       if (guess[i] === actualWord[i]) {
         newWord[i] = { letter: actualWord[i], color: "bg-green-500" };
-        
-      } else if (actualWord.includes(guess[i])) {
+      } else if (actualWord.includes(guess[i]) && !newWord.includes(guess[i])) {
         newWord[i] = { letter: guess[i], color: "bg-yellow-500" };
-       
       } else {
         newWord[i] = { letter: guess[i], color: "bg-gray-500" };
-    
       }
     }
     if (newWord.map((item) => item.letter).join("") === actualWord) {
-      toast.success("You have guessed the word correctly",{icon: "🎉"});
+      toast.success("You win!", { icon: "🎉" });
       setTimeout(() => {
         window.location.reload();
       }, 3000);
     }
-    //setWord(newWord);
-    setLines((prev) => {
-      prev[attempt] = newWord;
-      return prev;
-    });
+
+    
+      setLines((prev) => {
+        prev[attempt] = newWord;
+        return prev;
+      });
     setGuess("");
     setAttempt((prev) => prev + 1);
   }
   function handleSubmit(event: SyntheticEvent) {
-    event.preventDefault(); // Prevent the default form submission behavior
-    checkGuess(); // Call your checkGuess function
+    event.preventDefault();
+    checkGuess(); 
   }
 
+
   return (
-    <div>
-      <h1 className=" text-center p-5 text-6xl">Wordle clone</h1>
+    <div className="p-4 h-screen">
+    <div className="p-5">
+       <h1 className=" title text-center text-6xl font-extrabold text-cyan-400">Wordle Clone</h1>
+       <h2 className="text-center text-sm font-extrabold">(But not really..)</h2>
+    </div>
       <div className="flex flex-col gap-4 justify-center items-center align-middle">
-         <button
-            className=" absolute top-10 right-10 border rounded-md p-1"
-            onClick={() => window.location.reload()}
-          >
-            <IoReload />
-          </button>
+        <button
+          className=" absolute top-10 right-10 p-1 hover:scale-110 hover:animate-pulse"
+          onClick={() => window.location.reload()}
+        >
+          <IoReload size={25} />
+        </button>
         <form onSubmit={handleSubmit}>
           <input
             type="text"
-            placeholder="Guess the word!"
-            className="text-4xl w-100 h-20 placeholder:opacity-20 border-2 border-black text-center uppercase tracking-widest "
+            autoFocus={true}
+            placeholder="Guess"
+            className="text-4xl items-center rounded-md w-full p-8 h-20 placeholder:font-extrabold placeholder:scale-75 placeholder:opacity-20 border-2 border-black text-center uppercase tracking-widest "
             maxLength={5}
             value={guess}
             onChange={(e) => setGuess(e.target.value)}
           />
         </form>
         <div className="flex flex-row gap-2">
-           <div className="flex flex-col gap-2">
-          {lines.map((line, lineIndex) => (
-            <div key={lineIndex} className="flex flex-row gap-2">
-              {line.map((item, index) => (
-                <div
-                  key={index}
-                  className={`w-20 h-20 ${item.color} uppercase rounded-md shadow-md text-4xl text-black border flex justify-center items-center`}
-                >
-                  {item.letter}
-                </div>
-              ))}
-            </div>
-          ))}
+          <div className="flex flex-col gap-2">
+            {lines.map((line, lineIndex) => (
+              <div key={lineIndex} className="flex flex-row gap-2 slideDown">
+                {line.map((item, index) => (
+                  <div
+                    key={index}
+                    className={`w-16 h-16 ${item.color} uppercase rounded-md shadow-md text-4xl text-black border flex justify-center items-center  `}
+                  >
+                    {item.letter}
+                  </div>
+                ))}
+              </div>
+            ))}
+            {attempt === 4 && (
+              <h1 className="text-2xl text-center">Only one try left!</h1>
+            )}
+          </div> 
+        
         </div>
-        </div>
-      </div>
-    </div>
+      </div> 
+    </div> 
   );
 }
 
--- src/api/word.ts
+++ src/api/word.ts
@@ -10,3 +10,23 @@ export async function getRandomWord(){
     }   
 
 }
+
+export async function isItAWord(word: string): Promise<boolean> {
+    try {
+        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+        const data = await response.json();
+        if (Array.isArray(data) && data.length > 0) {
+            // Word exists and has definitions
+            return true;
+        } else if (data.title === "No Definitions Found") {
+            // No definitions found for the word
+            return false;
+        } else {
+            // Handle other cases if needed
+            return false;
+        }
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
